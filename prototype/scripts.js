af0db82b3cbcd6f661d880cd302b677dd19bb8a6
Forest wilson <hexaforest@gmail.com>
2022-11-12 14:12:31 -0500

    heldesk push, footer + render work

--- prototype/scripts.js
+++ prototype/scripts.js
@@ -1,1 +1,28 @@
-console.log("boner")
+console.log("commissioned by the IJA Burner Trust");
+
+(function(){
+    // pointers
+    const propaView = document.getElementById("propa");
+
+
+    // view swaps
+    const showHome = () => {
+        const heroFrag = document.createDocumentFragment();
+        const banDiv = document.createElement("div");
+        const panicBtn = document.createElement("span");
+
+        banDiv.classList.add("yealol");
+        panicBtn.classList.add("panic-btn");
+        panicBtn.classList.add("bait");
+
+        panicBtn.innerText = "AHHHHHHHHHHH!";
+
+        banDiv.appendChild(panicBtn);
+        heroFrag.appendChild(banDiv);
+
+        propaView.innerText = "";
+        propaView.appendChild(heroFrag);
+    }
+
+    showHome();
+})();
