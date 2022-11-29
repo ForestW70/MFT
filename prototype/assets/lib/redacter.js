2b8af2d55064f8ce192667997a2b7608a152c998
Forest wilson <hexaforest@gmail.com>
2022-11-28 20:15:41 -0500

    addition

--- prototype/assets/lib/redacter.js
+++ prototype/assets/lib/redacter.js
@@ -11,13 +11,13 @@ export const redacter = {
             titleAbr: "tmfmr2y",
             titleFull: "Transmissions From My Room 2 Yours",
             tracklength: "7:32",
-            src: ""
+            src: "https://burnthis.space/assets/music/RE-1-TFMR2Y.mp3"
         },
         {
             titleAbr: "lbr",
             titleFull: "Let's Be Reasonable",
             tracklength: "1:10",
-            src: ""
+            src: "https://burnthis.space/assets/music/RE-2-LBR.mp3"
         },
         {
             titleAbr: "dsgttctcotc?",
@@ -29,37 +29,37 @@ export const redacter = {
             titleAbr: "g,a16yowaar15a600roa/pf",
             titleFull: "Gee, Another 16 Year Old With An AR15 and 600 Rounds of Amunition / Pre Fukerz",
             tracklength: "4:07",
-            src: ""
+            src: "https://burnthis.space/assets/music/RE-4-PF.mp3"
         },
         {
             titleAbr: "fkk,cystrt?",
             titleFull: "Fukerz Killing Kids, Can't You See The Real Threats?",
             tracklength: "6:12",
-            src: ""
+            src: "https://burnthis.space/assets/music/RE-5-FKKCYSWTDYW.mp3"
         },
         {
             titleAbr: "ab",
             titleFull: "Amerika's Book",
             tracklength: "14:18",
-            src: ""
+            src: "https://burnthis.space/assets/music/RE-6-AB.mp3"
         },
         {
             titleAbr: "rs,ll,tc,troyg;toneas,ijwytktiwfy.",
             titleFull: "Richard Sackler, Leonard Leo, Tuckey C, The Rest Of You Gremlinz; This One's Not Even A Song, I Just Want You To Know.",
             tracklength: "05:15",
-            src: ""
+            src: "https://burnthis.space/assets/music/RE-7-MBS.mp3"
         },
         {
             titleAbr: "yr?/pd",
             titleFull: "Y Rent? / Pre Duets",
             tracklength: "14:19",
-            src: ""
+            src: "https://burnthis.space/assets/music/RE-8-YRPD.mp3"
         },
         {
             titleAbr: "d",
             titleFull: "Duets",
             tracklength: "6:15",
-            src: ""
+            src: "https://burnthis.space/assets/music/RE-9-D.mp3"
         }
     ]
 }
