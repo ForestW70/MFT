d0dadbdc78aed36e51598f5a8ab28e912ff86fb6
Forest wilson <hexaforest@gmail.com>
2022-12-08 23:15:51 -0500

    itjustaburner

---
+++ assets/lib/pageSwitch.js
@@ -0,0 +1,199 @@
+import { blogPosts } from "./blogs.js";
+import { flexTerms } from "./showTerms.js";
+import { flexCons } from "./constructionGifs.js";
+import { createPlayer } from "./showPlayer.js";
+import { newsArticles } from "./news.js";
+
+// view swaps
+
+const propaView = document.getElementById("propa");
+
+// Home View
+export const showHome = () => {
+    const propaFrag = document.createDocumentFragment();
+    const banDiv = document.createElement("div");
+    const panicBtn = document.createElement("span");
+    const consImg = document.createElement("img");
+
+    banDiv.classList.add("yealol");
+    consImg.src = "https://i.gifer.com/51L3.gif";
+    
+    banDiv.appendChild(consImg);
+    propaFrag.appendChild(banDiv);
+
+    propaView.innerText = "";
+    propaView.appendChild(propaFrag);
+}
+
+// About page view *empty*
+export const showAbout = () => {
+    const propaFrag = document.createDocumentFragment();
+    const consImg = document.createElement("img");
+    consImg.classList.add("under-con");
+    consImg.src = "https://c.tenor.com/X6i3RTlXpmAAAAAM/under-construction-men-at-work.gif";
+
+    const consText = document.createElement("span");
+    consText.innerText = "Page Under Construction! Come back soon ♥";
+    consText.classList.add("cons-blurb");
+
+    propaFrag.appendChild(consImg);
+    propaFrag.appendChild(consText);
+
+    propaView.innerText = "";
+    propaView.appendChild(propaFrag);
+}
+
+// News Articles view
+export const showNews = () => {
+    const propaFrag = document.createDocumentFragment();
+    let plsStopCnt = 0;
+    // render news obj
+    newsArticles.map(e => {
+        const newsCont = document.createElement("article");
+        const newsTit = document.createElement("h3");
+        const newsAbs = document.createElement("p");
+        const newsAuth = document.createElement("a");
+        const newsProp = document.createElement("span");
+
+        newsCont.classList.add("news-art");
+
+        newsTit.innerText = e.title;
+        newsAbs.innerText = e.body;
+        newsAuth.innerText = `${e.source}, ${e.author}, ${e.date}`;
+        newsProp.innerText = `shared by: ${e.poster}`;
+
+        newsAuth.href = e.link;
+        newsAuth.target = "blank";
+
+        newsCont.appendChild(newsTit);
+        newsCont.appendChild(newsAbs);
+        newsCont.appendChild(newsAuth);
+        newsCont.appendChild(newsProp);
+
+        if (Math.random() >= .5) {
+            propaFrag.appendChild(newsCont);
+
+            // plsStopCnt++;
+            // if (plsStopCnt === 10) {
+            //     const noNewsCont = document.createElement("article");
+            //     const blueText = document.createElement("p");
+            //     blueText.innerText = "please stop texting me";
+            //     blueText.classList.add("news-art", "its-you");
+            //     noNewsCont.appendChild(blueText);
+            //     propaFrag.appendChild(noNewsCont);
+            // } 
+        } else {
+            propaFrag.insertBefore(newsCont, propaFrag.firstChild);
+        }
+
+    });
+
+    propaView.innerText = "";
+    propaView.appendChild(propaFrag);
+}
+
+// Redacter view
+export const showVideos = () => {
+    const propaFrag = document.createDocumentFragment();
+    propaFrag.appendChild(createPlayer());
+
+    propaView.innerText = "";
+    propaView.appendChild(propaFrag);
+}
+
+// blog post view
+export const showBlog = () => {
+    const propaFrag = document.createDocumentFragment();
+    const viewOrder = document.createElement("p");
+    viewOrder.classList.add("blog-order");
+    viewOrder.innerText = "Showing newest first";
+    
+    // change view order
+    let viewVar = 1;
+    viewOrder.addEventListener("click", () => {
+        if (viewVar === 1) {
+            viewVar = 0;
+            viewOrder.innerText = "Showing oldest first";
+           
+        } else {
+            viewVar = 1;
+            viewOrder.innerText = "Showing newest first";
+
+        }
+
+        propaView.innerText = "";
+        splayBlogs();
+
+    })
+
+
+    // render blog obj
+    const splayBlogs = () => {
+        blogPosts.map(e => {
+            const pTitle = document.createElement("h3");
+            const pAuth = document.createElement("span");
+            const pImg = document.createElement("img");
+            const pBody = document.createElement("p");
+
+            pTitle.innerText = e.postTitle;
+            pAuth.innerText = `Posted by: ${e.postAuth} on ${e.postDate}`;
+            pImg.src = e.postImg;
+            pBody.innerText = e.postBody;
+
+            const pCont = document.createElement("article");
+            pCont.classList.add("hide"); //
+            pCont.classList.add("blog-post");
+            pCont.appendChild(pTitle);
+            pCont.appendChild(pAuth);
+            pCont.appendChild(pImg);
+            e.postBody.map(ee => {
+                const newParagraph = document.createElement("p");
+                newParagraph.innerText = ee.par;
+                pCont.appendChild(newParagraph);
+
+            });
+
+            // change render dir based on var
+            viewVar === 0 ? propaFrag.insertBefore(pCont, propaFrag.firstChild) : propaFrag.appendChild(pCont);
+            
+        });
+        const hider = document.createElement("div");
+        hider.classList.add("hide-blog");
+        const myConstruc = document.createElement("img");
+        myConstruc.src = "https://thumbs.gfycat.com/SleepySadAegeancat-max-1mb.gif";
+        const consMessage = document.createElement("p");
+        consMessage.innerText = "We'll be right back! -♥Admin";
+        
+        hider.appendChild(myConstruc);
+        hider.appendChild(consMessage);
+
+        propaView.appendChild(hider);
+        propaView.appendChild(viewOrder);
+        propaView.appendChild(propaFrag)
+    }
+
+    propaView.innerText = "";
+    splayBlogs();
+}
+
+// fuck around view
+export const showContact = () => {
+    const propaFrag = document.createDocumentFragment();
+    const consImg = flexCons();
+    propaFrag.appendChild(consImg);
+
+    propaView.innerText = "";
+    propaView.appendChild(propaFrag);
+}
+
+// terms and privacy view
+export const showPriv = () => {
+    const pageTitle = document.createElement("p");
+    pageTitle.innerText = "Terms, Conditions, and Privacy"
+
+    const newFill = flexTerms();
+
+    propaView.innerText = "";
+    propaView.appendChild(pageTitle);
+    propaView.appendChild(newFill);
+}
---
+++ assets/lib/redacter.js
@@ -0,0 +1,65 @@
+export const redacter = {
+    albumTitle: "Redacter",
+    albumArtist: "L. Mauro",
+    albumArt: "https://forestw70.github.io/sketches-bcc-client/assets/images/unredacted.png",
+    rLabel: "Burner Records",
+    releaseDate: "November 8th 2022",
+    trackTotal: 9,
+    timeTotal: "64:58",
+    tracklist: [
+        {
+            titleAbr: "tmfmr2y",
+            titleFull: "Transmissions From My Room 2 Yours",
+            tracklength: "7:32",
+            src: "https://burnthis.space/assets/music/RE-1-TFMR2Y.mp3"
+        },
+        {
+            titleAbr: "lbr",
+            titleFull: "Let's Be Reasonable",
+            tracklength: "1:10",
+            src: "https://burnthis.space/assets/music/RE-2-LBR.mp3"
+        },
+        {
+            titleAbr: "dsgttctcotc?",
+            titleFull: "Does Slitting Ginni Thomas's Throat Count Towards Carbon Offset Tax Credits?",
+            tracklength: "12:50",
+            src: "https://forestw70.github.io/sketches-bcc-client/assets/music/RE-3-DSGTTCTCOTC.mp3"
+        },
+        {
+            titleAbr: "g,a16yowaar15a600roa/pf",
+            titleFull: "Gee, Another 16 Year Old With An AR15 and 600 Rounds of Amunition / Pre Fukerz",
+            tracklength: "4:07",
+            src: "https://burnthis.space/assets/music/RE-4-PF.mp3"
+        },
+        {
+            titleAbr: "fkk,cystrt?",
+            titleFull: "Fukerz Killing Kids, Can't You See The Real Threats?",
+            tracklength: "6:12",
+            src: "https://burnthis.space/assets/music/RE-5-FKKCYSWTDYW.mp3"
+        },
+        {
+            titleAbr: "ab",
+            titleFull: "Amerika's Book",
+            tracklength: "14:18",
+            src: "https://burnthis.space/assets/music/RE-6-AB.mp3"
+        },
+        {
+            titleAbr: "rs,ll,tc,troyg;toneas,ijwytktiwfy.",
+            titleFull: "Richard Sackler, Leonard Leo, Tuckey C, The Rest Of You Gremlinz; This One's Not Even A Song, I Just Want You To Know.",
+            tracklength: "05:15",
+            src: "https://burnthis.space/assets/music/RE-7-MBS.mp3"
+        },
+        {
+            titleAbr: "yr?/pd",
+            titleFull: "Y Rent? / Pre Duets",
+            tracklength: "14:19",
+            src: "https://burnthis.space/assets/music/RE-8-YRPD.mp3"
+        },
+        {
+            titleAbr: "d",
+            titleFull: "Duets",
+            tracklength: "6:15",
+            src: "https://burnthis.space/assets/music/RE-9-D.mp3"
+        }
+    ]
+}
